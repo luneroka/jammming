7f0b80b087a2da70cd61ddf5405da66a5804a763
Yoann Robert <luneroka@gmail.com>
2025-01-17 12:07:40 +0100

    Create SearchResults component with initial return statement

---
+++ frontend/src/components/SearchResults.jsx
@@ -0,0 +1,12 @@
+import Tracklist from './TrackList';
+
+function SearchResults() {
+  return (
+    <div className='search-results'>
+      <h2>Results</h2>
+      <Tracklist />
+    </div>
+  );
+}
+
+export default SearchResults;
