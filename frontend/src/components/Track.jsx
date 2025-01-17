9e429ceeff9dd61d016423eb61da5eb51633f842
Yoann Robert <luneroka@gmail.com>
2025-01-17 16:11:22 +0100

    Define addTrack and removeTrack handlers in Track.jsx

--- frontend/src/components/Track.jsx
+++ frontend/src/components/Track.jsx
@@ -1,6 +1,21 @@
+import { useCallback } from 'react';
 import '../css/Track.css';
 
 function Track(props) {
+  const addTrack = useCallback(
+    (event) => {
+      props.onAdd(props.track);
+    },
+    [props.onAdd, props.track]
+  );
+
+  const removeTrack = useCallback(
+    (event) => {
+      props.onRemove(props.track);
+    },
+    [props.onRemove, props.track]
+  );
+
   return (
     <div className='track'>
       <div className='track-information'>
